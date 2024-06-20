f85d3d7a8a18d09c0ff8eb07f1ef5eb0b2c0aa7f
Siddharthcoding <siddharthkumarmishra1011@gmail.com>
2024-06-20 22:09:10 +0530

    creating a mini custom react library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from "./chai"
 
 
 function App() {
+  const username = "Siddharth"
 
   return (  // can return only 1 element, wrapping it in a fragment
+  // only evaluated expression can be injected not whole expression like if-else
     <>      
       <Chai/>
-      <h1>Chai aur react</h1>
+      <h1>Chai aur react {username}</h1>  
       <p>Test para</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){  // parsed into a html object
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href = "https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "Sid"
+
+const reactElement = React.createElement(
+  'a',      //tag
+  {href: 'https://google.com', target: '_blank'},  // attributes
+  'click me to visit google',    // text
+  anotherElement
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <MyApp />
+    anotherElement
+    reactElement
   </React.StrictMode>,
 )
